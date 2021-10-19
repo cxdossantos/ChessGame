52ff1f62e194460d455d44847d14073074f4984d
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-10-19 13:29:17 -0300

    Comecando colocar pecas no tabuleiro

--- ChessGame/tabuleiro/Peca.cs
+++ ChessGame/tabuleiro/Peca.cs
@@ -13,12 +13,12 @@ namespace tabuleiro
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro tabuleiro  { get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        public Peca(Tabuleiro tabuleiro, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.tabuleiro = tabuleiro;
             this.cor = cor;
-            qtdMovimentos = 0;
+            this.qtdMovimentos = 0;
 
         }
     }

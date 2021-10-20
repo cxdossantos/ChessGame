077b8ee8de3471fe0987682896c1f7bba5e8143d
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-10-20 11:26:08 -0300

    Melhorano implementação do método ImprimirTabuleiro

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -13,9 +13,11 @@ namespace ChessGame
 
             tab.colocarPeca(new Torre(tab, Cor.Preta) , new Posicao(0, 0));
             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
-            Tela.imprimirTabuleiro(tab);
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(7, 2));
+                Tela.imprimirTabuleiro(tab);
             }
             catch(TabuleiroException e)
             {

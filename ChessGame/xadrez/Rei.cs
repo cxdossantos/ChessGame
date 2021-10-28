1c61656fb6bacb5a9b7ede2c7b959bd3471dad24
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-10-28 11:18:49 -0300

    Jogada especial ROQUE GRANDE e ROQUE PEQUENO

--- ChessGame/xadrez/Rei.cs
+++ ChessGame/xadrez/Rei.cs
@@ -4,9 +4,11 @@ namespace xadrez
 {
     class Rei : Peca
     {
+        private PartidaDeXadrez partida;
 
-        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
         {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -19,6 +21,12 @@ namespace xadrez
             return p == null || p.cor != this.cor;
         }
 
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tabuleiro.peca(pos);
+            return p != null && p is Torre && p.cor == cor;
+        }
+
 
         public override bool[,] movimentosPossiveis()
         {
@@ -28,7 +36,7 @@ namespace xadrez
 
             //acima
             pos.definirValores(posicao.linha - 1, posicao.coluna);
-            if(tabuleiro.posicaoValida(pos) && podeMover(pos))
+            if (tabuleiro.posicaoValida(pos) && podeMover(pos))
             {
                 mat[pos.linha, pos.coluna] = true;
             }
@@ -74,6 +82,36 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
             }
+
+            // #JogadaEspecial: ROQUE
+            if (qtdMovimentos == 0 && !partida.xeque)
+            {
+                // #JogadaEspecial: Roque PEQUENO
+                Posicao posT1 = new Posicao(posicao.linha, posicao.coluna + 3);
+                if (testeTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna + 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna + 2);
+                    if (tabuleiro.peca(p1) == null && tabuleiro.peca(p2) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna + 2] = true;
+                    }
+                }
+
+                // #JogadaEspecial: Roque GRANDE
+                Posicao posT2 = new Posicao(posicao.linha, posicao.coluna - 4);
+                if (testeTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(posicao.linha, posicao.coluna - 1);
+                    Posicao p2 = new Posicao(posicao.linha, posicao.coluna - 2);
+                    Posicao p3 = new Posicao(posicao.linha, posicao.coluna - 3);
+                    if (tabuleiro.peca(p1) == null && tabuleiro.peca(p2) == null && tabuleiro.peca(p3) == null)
+                    {
+                        mat[posicao.linha, posicao.coluna - 2] = true;
+                    }
+                }
+            }
+
             return mat;
         }
     }

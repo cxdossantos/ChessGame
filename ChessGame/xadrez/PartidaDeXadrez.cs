45ced97f5395a200474fc9bbce15da8f6ebfb8d1
Cleiton Xavier dos Santos <cxdosantos@gmail.com>
2021-10-28 17:53:09 -0300

    Jogada Especial Promocao

--- ChessGame/xadrez/PartidaDeXadrez.cs
+++ ChessGame/xadrez/PartidaDeXadrez.cs
@@ -138,6 +138,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Jogador não pode se colocar em Xeque! ");
             }
+
+            Peca p = tab.peca(destino);
+
+            // #JogadaEspecial: Promoção
+            if(p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -156,8 +172,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // #JogadaEspecial: En Passant 
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
